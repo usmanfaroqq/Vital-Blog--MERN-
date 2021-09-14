8891dc8b6c5a4e06b0cc89153a14b04f05cbb96a
Md. Osman Faruk <programmming.code@gmail.com>
2021-09-14 20:05:59 +0600

    halfDesigndone

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -5,26 +5,32 @@ import { REDIRECT_FALSE, REMOVE_MESSAGE } from "../../redux/types/PostTypes";
 import toast, { Toaster } from "react-hot-toast";
 import swal from "sweetalert";
 import { fetchPosts } from "../../redux/asyncMethods/PostMethods";
+import { Col, Container, Row , Button} from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+
 const Dashboard = () => {
-  const { redirect, message, loading } = useSelector((state) => state.PostReducer);
-  const {user : {_id}} = useSelector((state) => state.AuthReducer)
-  const {posts} = useSelector((state) => state.FetchPosts)
+  const { redirect, message, loading } = useSelector(
+    (state) => state.PostReducer
+  );
+  const {
+    user: { _id },
+  } = useSelector((state) => state.AuthReducer);
+  const { posts } = useSelector((state) => state.FetchPosts);
   const dispatch = useDispatch();
-  console.log('ok', posts)
+  console.log("ok", posts);
   useEffect(() => {
     if (redirect) {
       dispatch({ type: REDIRECT_FALSE });
     }
-    if(message) {
-      toast.success(message , {
+    if (message) {
+      toast.success(message, {
         duration: 4000,
       });
       dispatch({ type: REMOVE_MESSAGE });
     }
     dispatch(fetchPosts(_id));
   }, []); // Showing post confirm message
-
-  
 
   return (
     <>
@@ -44,7 +50,25 @@ const Dashboard = () => {
           },
         }}
       />
-      <h1>Dashboard</h1>
+      <Container>
+        <Row>
+          <div className="dashboard">
+            <h1>Your blogs</h1>
+            
+          
+          <div className="dashboard__body">
+            <div className="dashboard__body-header d-flex gap-4">
+            <h4>Published</h4>
+            <h4><Link to="/new-post" style={{textDecoration:'none'}}>Create new blog</Link></h4>
+            </div>
+          <Col md={12}>
+            
+          </Col>
+
+          </div>  
+          </div>
+        </Row>
+      </Container>
     </>
   );
 };
